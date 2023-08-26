cb314fca3bb4174c01ebd9108f75ff5b81e187c5
Senior-Bogatyrev <jusuf94@mail.ru>
2023-08-26 07:20:49 +0300

    view beginning

---
+++ app.js
@@ -0,0 +1,27 @@
+const loadData = function(search, cb) {
+    let request = new XMLHttpRequest();
+    let url = 'https://api.kinopoisk.dev/v1.2/movie/search?page=1&limit=10&query=' + search;
+    request.open('GET', url);
+    request.setRequestHeader('X-API-KEY', 'ВВЕДИТЕ СВОЙ API-KEY');
+    request.responseType = 'json'
+    request.addEventListener(`load`, cb);
+    request.send();
+}
+
+let searchBtn = document.getElementById('searchBtn');
+let films = document.getElementById('films');
+
+function searcher(){
+    let search = document.getElementById('title').value;
+    loadData(search, function(evt){
+        films.innerHTML = ''
+        const result = evt.currentTarget.response;
+        for (let i = 0; i < result['docs'].length; i++) {
+            if (result['docs'][i]['poster'] == null){
+                continue
+            }else{
+            films.innerHTML += `<div class="film"><div class="card"><div class="poster"><img src="${result['docs'][i]['poster']}" alt="poster" id="miniPoster"></div><div class="info"><h3 id="name">${result['docs'][i]['name']}</h3><br><h3 id="year">2015</h3><button class="details" id="${i}">Details</button></div></div></div>`
+            }
+        }
+    });  
+}
